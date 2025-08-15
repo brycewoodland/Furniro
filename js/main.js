90000978dc21fa385232ee604f6b216ac8eed342
Bryce Woodland <bwoodland@vivacitytech.com>
2025-08-14 18:51:03 -0600

    Added Hamburger Menu

--- js/main.js
+++ js/main.js
@@ -3,3 +3,20 @@ const copyrightDate = document.querySelector('.copyright-date')
 
 const date = new Date().getFullYear();
 copyrightDate.textContent = date;
+
+// Hamburger Menu
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+const mainNav = document.querySelector('.main-nav');
+const navLinks = document.querySelectorAll('a');
+
+hamburgerMenu.addEventListener('click', () => {
+    hamburgerMenu.classList.toggle('open');
+    mainNav.classList.toggle('open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        hamburgerMenu.classList.remove('open');
+        mainNav.classList.remove('open');
+    })
+})
