4648765735d4d5b4882c40118189358bbcef28e4
Bryce Woodland <bwoodland@vivacitytech.com>
2025-08-15 13:44:57 -0600

    Working on the card slider

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@ copyrightDate.textContent = date;
 
 // Hamburger Menu
 const body = document.body;
-const hamburgerMenu = document.querySelector('.hamburger-menu');
+const hamburgerMenu = document.querySelector('[data-nav-toggle]');
 const mainNav = document.querySelector('.main-nav');
 const navLinks = document.querySelectorAll('a');
 
@@ -21,5 +21,35 @@ navLinks.forEach(link => {
         hamburgerMenu.classList.remove('open');
         mainNav.classList.remove('open');
         body.classList.remove('no-scroll');
-    })
-})
+    });
+});
+
+// Card Slider
+const cards = document.querySelectorAll('.card-inspiration');
+const descriptions = document.querySelectorAll('.card-description');
+const buttons = document.querySelectorAll('.next-btn');
+const arrow = document.querySelector('.arrow-badge');
+const track = document.querySelector('.card-track');
+const cardWidth = cards[0].offsetWidth;
+let currentIndex = 0;
+
+const updateActiveCard = (index) => {
+  cards.forEach((card, i) => {
+    descriptions[i].classList.remove('active');
+    buttons[i].classList.remove('active');
+    card.classList.remove('active');
+  });
+
+  descriptions[index].classList.add('active');
+  buttons[index].classList.add('active');
+  cards[index].classList.add('active');
+
+  track.style.transform = `translateX(-${index * cardWidth * 1.1}px)`;
+}
+
+arrow.addEventListener('click', () => {
+  currentIndex = (currentIndex + 1) % cards.length;
+  updateActiveCard(currentIndex);
+});
+
+updateActiveCard(currentIndex);
