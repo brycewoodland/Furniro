d3874b7c51df949f3804827e4829e1e11b708959
Bryce Woodland <bwoodland@vivacitytech.com>
2025-08-14 21:51:41 -0600

    Updated mobile view.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,7 @@ const date = new Date().getFullYear();
 copyrightDate.textContent = date;
 
 // Hamburger Menu
+const body = document.body;
 const hamburgerMenu = document.querySelector('.hamburger-menu');
 const mainNav = document.querySelector('.main-nav');
 const navLinks = document.querySelectorAll('a');
@@ -12,11 +13,13 @@ const navLinks = document.querySelectorAll('a');
 hamburgerMenu.addEventListener('click', () => {
     hamburgerMenu.classList.toggle('open');
     mainNav.classList.toggle('open');
+    body.classList.toggle('no-scroll');
 });
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
         hamburgerMenu.classList.remove('open');
         mainNav.classList.remove('open');
+        body.classList.remove('no-scroll');
     })
 })
